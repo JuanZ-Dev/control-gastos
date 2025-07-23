a33a57618efa42526ebd9789a4242868c2301358
JuanZ-Dev <juan.z.981611@gmail.com>
2025-07-23 00:07:38 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h2 className="uppercase text-center font-black text-4xl text-white">
+          Planificador de Gastos
+        </h2>
+      </header>
+
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        <p>Formulario aquí</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
