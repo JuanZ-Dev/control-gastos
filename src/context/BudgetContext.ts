e2b5b0f425cc90fd210649f4a5698315c63c2d4d
JuanZ-Dev <juan.z.981611@gmail.com>
2025-08-03 21:31:42 -0500

    separate the context into two files

---
+++ src/context/BudgetContext.ts
@@ -0,0 +1,12 @@
+import { createContext, type Dispatch } from "react";
+import {
+  type BudgetActions,
+  type BudgetState,
+} from "../reducers/budget-reducer";
+
+type BudgetContextProps = {
+  state: BudgetState;
+  dispatch: Dispatch<BudgetActions>;
+};
+
+export const BudgetContext = createContext<BudgetContextProps>(null!);
