867ed2eca138136806eb9aadc5243005aa3a988b
JuanZ-Dev <juan.z.981611@gmail.com>
2025-08-11 21:17:37 -0500

    add functionality to the progress chart, and the action to reset the application

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,19 +1,33 @@
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
 
 export default function BudgetTracker() {
-  const { state, totalExpenses, remainingBudget } = useBudget();
+  const { state, dispatch, totalExpenses, remainingBudget } = useBudget();
+
+  const percentage = Number(((totalExpenses / state.budget) * 100).toFixed(2));
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
       <div className="flex justify-center">
-        <img src="/grafico.jpg" alt="Gráfica de gastos" />
+        <CircularProgressbar
+          value={percentage}
+          styles={buildStyles({
+            pathColor: percentage === 100 ? "#dc2626" : "#3b82f6",
+            trailColor: "#f5f5f5",
+            textSize: 8,
+            textColor: percentage === 100 ? "#dc2626" : "#3b82f6",
+          })}
+          text={`${percentage}% Gastado`}
+        />
       </div>
 
       <div className="flex flex-col justify-center items-center gap-8">
         <button
           type="button"
           className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg cursor-pointer"
+          onClick={() => dispatch({ type: "reset-app" })}
         >
           Resetear App
         </button>
